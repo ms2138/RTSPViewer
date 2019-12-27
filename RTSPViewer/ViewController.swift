ec2437f41d33034955e9e7972b9804f033725e5a
ms2138 <m38371@gmail.com>
2019-12-26 18:49:51 -0800

    Initial commit

---
+++ RTSPViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTSPViewer
+//
+//  Created by mani on 2019-12-26.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
