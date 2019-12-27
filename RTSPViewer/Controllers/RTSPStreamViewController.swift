71a8068ebe26325578e854072c3fb3d4db0ac59e
ms2138 <m38371@gmail.com>
2019-12-26 23:56:57 -0800

    Add Controllers group.  Add RTSPStreamController collection view class to project.

---
+++ RTSPViewer/Controllers/RTSPStreamViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RTSPStreamViewController.swift
+//  RTSPViewer
+//
+//  Created by mani on 2019-12-26.
+//  Copyright © 2019 mani. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class RTSPStreamViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+    }
+}
+
+extension RTSPStreamViewController {
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+
+        // Configure the cell
+
+        return cell
+    }
+}
