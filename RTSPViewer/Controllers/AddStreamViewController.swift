55911d233f538fbf8bc3e9334dd6a6eb5867ccd5
ms2138 <m38371@gmail.com>
2020-01-01 20:24:27 -0800

    Add AddStreamViewController class to project

---
+++ RTSPViewer/Controllers/AddStreamViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AddStreamViewController.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-01.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class AddStreamViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
