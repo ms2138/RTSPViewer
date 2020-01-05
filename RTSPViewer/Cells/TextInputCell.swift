d67b20f4294281c7fbf606633309dcb540649c00
ms2138 <m38371@gmail.com>
2020-01-05 00:27:08 -0800

    Add textField property and initialization methods to TextInputCell

--- RTSPViewer/Cells/TextInputCell.swift
+++ RTSPViewer/Cells/TextInputCell.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 class TextInputCell: UITableViewCell {
+    let textField: UITextField
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        textField = UITextField()
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        initialize()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        textField = UITextField()
+
+        super.init(coder: aDecoder)
+
+        initialize()
+    }
+
+    private func initialize() {
+        textField.clearButtonMode = .whileEditing
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.addSubview(textField)
+    }
 }
