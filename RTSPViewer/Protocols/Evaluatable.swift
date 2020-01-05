8c075dfd425dc1234dfe3697677fac66289f2e37
ms2138 <m38371@gmail.com>
2020-01-05 15:08:44 -0800

    Add Evaluatable protocol to project.  Add Expression associatedtype and evaluate method to Evaluatable protocol

---
+++ RTSPViewer/Protocols/Evaluatable.swift
@@ -0,0 +1,15 @@
+//
+//  Evaluatable.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-05.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import Foundation
+
+protocol Evaluatable {
+    associatedtype Expression
+
+    func evaluate(with condition: Expression) -> Bool
+}
