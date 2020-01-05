d709455419be8047c58585ef3bae839e2bfc281b
ms2138 <m38371@gmail.com>
2020-01-05 00:23:51 -0800

    Add TextInputCell class to project.  Add Cells folder.  Move TextInputCell into Cells folder

---
+++ RTSPViewer/Cells/TextInputCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextInputCell.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-05.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class TextInputCell: UITableViewCell {
+
+}
