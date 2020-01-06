1f17b863aca90774c9ffc7215836b7fc626d9985
ms2138 <m38371@gmail.com>
2020-01-05 22:07:23 -0800

    Add handleTextfieldValidation and done methods to AddStreamViewController

--- RTSPViewer/Controllers/AddStreamViewController.swift
+++ RTSPViewer/Controllers/AddStreamViewController.swift
@@ -28,3 +28,41 @@ class AddStreamViewController: UITableViewController {
         return 0
     }
 }
+
+extension AddStreamViewController {
+    // MARK: - Text field validation
+
+    func handleTextfieldValidation(in textField: UITextField, message: String) {
+        textField.text = ""
+        let placeholderTextColor = UIColor(red: 236.0/255.0, green: 75.0/255.0, blue: 75.0/255.0, alpha: 1.0)
+        textField.attributedPlaceholder = NSAttributedString(string: message,
+                                                             attributes:
+            [NSAttributedString.Key.foregroundColor: placeholderTextColor])
+        textField.textColor = .red
+        textField.shake()
+
+        textField.becomeFirstResponder()
+    }
+}
+
+
+extension AddStreamViewController {
+    // MARK: - IBAction
+
+    @IBAction func done() {
+        let textField = urlCell.textField
+        let validator = Validator()
+
+        if textField.validate([validator.isURLValid]) == false {
+            handleTextfieldValidation(in: textField,
+                                      message: "Please enter a valid URL")
+        }
+
+        if let url = textField.text {
+            if let url = URL(string: url) {
+                handler?(url)
+            }
+        }
+    }
+}
+
