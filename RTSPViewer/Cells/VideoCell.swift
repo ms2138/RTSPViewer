efa3188192840614f1e01266e6c9b924a5cb2910
ms2138 <m38371@gmail.com>
2020-01-07 22:33:46 -0800

    Add VideoCell class to project

---
+++ RTSPViewer/Cells/VideoCell.swift
@@ -0,0 +1,13 @@
+//
+//  VideoCell.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-07.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class VideoCell: UICollectionViewCell {
+
+}
