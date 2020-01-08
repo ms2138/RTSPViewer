e5578aa0f7f5433982933194d26e7edfaca2b703
ms2138 <m38371@gmail.com>
2020-01-07 21:51:04 -0800

    Add RTSPViewer-Bridging-Header bridging header.  Add bridging header path in build settings.  Add MobileVLCKit headers to RTSPViewer-Bridging-Header

---
+++ RTSPViewer/RTSPViewer-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  RTSPViewer-Bridging-Header.h
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-07.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+#import <MobileVLCKit/MobileVLCKit.h>
