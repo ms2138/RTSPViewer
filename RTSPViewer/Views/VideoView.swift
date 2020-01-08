f33290a1bfc752ca80271f13da387c659d22970a
ms2138 <m38371@gmail.com>
2020-01-07 21:45:43 -0800

    Add VideoView class.  Add Views folder.  Move VideoView into Views folder

---
+++ RTSPViewer/Views/VideoView.swift
@@ -0,0 +1,13 @@
+//
+//  VideoView.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-07.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class VideoView: UIView {
+
+}
