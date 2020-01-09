f55924be3c57cf9d25c2c573425597d4c7dbc46f
ms2138 <m38371@gmail.com>
2020-01-08 22:57:55 -0800

    Add videoView property.  Add init methods to initialize variables in VideoCell

--- RTSPViewer/Cells/VideoCell.swift
+++ RTSPViewer/Cells/VideoCell.swift
@@ -3,11 +3,33 @@
 //  RTSPViewer
 //
 //  Created by mani on 2020-01-07.
-//  Copyright © 2020 home. All rights reserved.
+//  Copyright © 2020 mani. All rights reserved.
 //
 
 import UIKit
 
 class VideoCell: UICollectionViewCell {
+    var videoView: VideoView
 
+    override init(frame: CGRect) {
+        videoView = VideoView(frame: frame)
+
+        super.init(frame: frame)
+
+        initialize()
+    }
+
+    required init?(coder: NSCoder) {
+        videoView = VideoView(frame: .zero)
+
+        super.init(coder: coder)
+
+        initialize()
+    }
+
+    private func initialize() {
+        videoView.aspectRatio = "1:1"
+
+        contentView.addSubview(videoView)
+    }
 }
