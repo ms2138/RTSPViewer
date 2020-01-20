d0ab055aa70ffa7c166b55388ccfcfd6329e12bd
ms2138 <m38371@gmail.com>
2020-01-19 19:51:18 -0800

    Change Validator's isURLValid method regex to support HTTP, HTTPS, RTSP

--- RTSPViewer/Validator.swift
+++ RTSPViewer/Validator.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Validator {
     func isURLValid(text: String) -> Bool {
-        let regexp = "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$"
+        let regexp = "(?i)(http?|https|rtsp)://(?:www\\.)?\\S+(?:/|\\b)"
         return text.evaluate(with: regexp)
     }
 }
