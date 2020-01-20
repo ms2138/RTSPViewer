26f5dd0bcb738877d1b38e251c580be53349cc07
ms2138 <m38371@gmail.com>
2020-01-19 20:10:14 -0800

    Add collection view drag and drop functionality to RTSPStreamViewController

--- RTSPViewer/Controllers/RTSPStreamViewController.swift
+++ RTSPViewer/Controllers/RTSPStreamViewController.swift
@@ -16,6 +16,10 @@ class RTSPStreamViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        collectionView.dragInteractionEnabled = true
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
     }
 }
 
@@ -64,6 +68,59 @@ extension RTSPStreamViewController: UICollectionViewDelegateFlowLayout {
         let cellDimensions = CGSize(width: view.frame.width / 2,
                                     height: view.frame.width / 2)
         return cellDimensions
+    }
+}
+
+extension RTSPStreamViewController: UICollectionViewDragDelegate {
+    // MARK: - Collection view drag delegate
+
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession,
+                        at indexPath: IndexPath) -> [UIDragItem] {
+        let url = videoStreams[indexPath.row]
+
+        let itemProvider = NSItemProvider(object: url as NSURL)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        return [dragItem]
+    }
+}
+
+extension RTSPStreamViewController: UICollectionViewDropDelegate {
+    // MARK: - Collection view drop delegate
+
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return true
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        performDropWith coordinator: UICollectionViewDropCoordinator) {
+        guard let destinationIndexPath = coordinator.destinationIndexPath else {
+            return
+        }
+
+        coordinator.items.forEach { dropItem in
+            guard let sourceIndexPath = dropItem.sourceIndexPath else {
+                return
+            }
+
+            collectionView.performBatchUpdates({
+                let toMoveStream = videoStreams.remove(at: sourceIndexPath.row)
+                videoStreams.insert(toMoveStream, at: destinationIndexPath.row)
+
+                collectionView.deleteItems(at: [sourceIndexPath])
+                collectionView.insertItems(at: [destinationIndexPath])
+            }, completion: { _ in
+                coordinator.drop(dropItem.dragItem,
+                                 toItemAt: destinationIndexPath)
+            })
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession,
+                        withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        if collectionView.hasActiveDrag {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        return UICollectionViewDropProposal(operation: .forbidden)
     }
 }
 
