6b580ab7e23ada7b032a748882beaa67d635f41a
ms2138 <m38371@gmail.com>
2020-01-26 19:55:20 -0800

    Add VideoStreamController class

---
+++ RTSPViewer/Controllers/VideoStreamController.swift
@@ -0,0 +1,16 @@
+//
+//  VideoStreamController.swift
+//  RTSPViewer
+//
+//  Created by mani on 2020-01-26.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class VideoStreamController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
